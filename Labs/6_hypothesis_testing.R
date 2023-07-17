df5e1c9a20fbeb362bb246640c37798fc02a98b2
Mone Abbott <moneabbott@gmail.com>
2023-07-17 15:15:18 -0400

    Update: Lab 5 -7

--- Labs/6_hypothesis_testing.R
+++ Labs/6_hypothesis_testing.R
@@ -2,6 +2,15 @@
 ### Null Hypothesis : average mass of male and female star wars character is the sma
 ### alternative hypothesis : average mass of female star wars charactesr is different 
 
+# load in library 
+library(dplyr)
+library(ggplot2)
+
+# load in package 
+install.packages("corrplot")
+library(corrplot)
+
+# star wars 
 swHuman <-starwars |> filter(species == "Human", mass > 0)
 males <- swHuman |> filter(sex =="male")
 females <- swHuman |> filter(sex == "females")
@@ -12,3 +21,99 @@ males
 
 # incorrect sytax aka gives an error
 t.test(males$mass, females$mass, paired = F, alternative = "two.sided")
+
+
+### ANOVA #####
+iris
+anova_results= aov(data = iris, iris$Sepal.Width ~ iris$Species)
+
+# Are any groups different from each other?
+summary(anova_results)
+
+# WHich ones?
+TukeyHSD(anova_results)
+
+
+# Is there a significant difference in the mean petal lenghts or petal width by species? 
+nova_results = aov(data = iris, iris$Petal.Length ~ iris$Species)
+
+summary(nova_results)
+
+TukeyHSD(nova_results)
+
+### Starwars ####
+head(starwars)
+unique(starwars$species)
+
+# which 3 species are the most common 
+top5species = starwars |> 
+  summarize(.by = species, count = sum(!is.na(species))) |>
+              slice_max(count, n=3)
+top5species  
+
+starwars_3 = -8
+
+## is there a signifcant difference in the mass of each of the top 3 species 
+a = aov(height ~ species, )
+
+
+
+
+
+### Chi-Squared ####
+
+# clean-up data 
+starwars_cleanup = starwars |> 
+  filter(!is.na(species), 
+         !is.na(homeworld))
+
+con_table = table(starwars$species, starwars$homeworld)
+
+View(con_table)
+
+# make chisq 
+
+chisq.test(con_table)
+
+
+### NEw dataset 
+# Make tables for year and drive 
+mpg
+
+t_tb = table(mpg$year, mpg$drv)
+
+chisq_result = chisq.test(t_tb)
+chisq_result
+chisq_result$p.value
+chisq_result$residuals
+
+# visual plot! 
+corrplot(chisq_result$residuals)
+
+
+### Chi- squared full analysis ####
+
+hereos = read.csv("/Users/tamiaabbott/Desktop/programing/camps/DSRP/DSRP_2023/Data/heroes_information.csv")
+head(hereos)
+
+# cleaning data :) 
+hereos_clean = hereos |> 
+  filter(Alignment != "-",
+         Gender != "-")
+
+
+## plot the counts of the alignment and gender 
+ggplot(hereos_clean, aes(x = Gender, y = Alignment)) + 
+  geom_count() + 
+  theme_minimal()
+
+# make contingency table 
+t_hero = table(hereos_clean$Alignment, hereos_clean$Gender)
+
+# chi - sqaured test 
+chi = chisq.test(t_hero)
+chi$p.value
+chi$residuals
+
+# plotting the chi - sqaure 
+corrplot(chi$residuals, is.corr = F )
---
+++ notes /7_machine-learning.R
@@ -0,0 +1,183 @@
+## packages ####
+install.packages("tidymodels")
+install.packages("reshape2")
+install.packages("xgboost")
+install.packages("ranger")
+install.packages("MLmetrics")
+library(ranger)
+library(reshape2)
+library(yardstick)
+library(MLmetrics)
+
+## Unsupervised learning #### 
+## Principal Components Analysis 
+head(iris)
+
+
+## remove any non-numeric variables 
+iris_num = select(iris, -Species)
+iris_num
+
+## do PCA
+pcas = prcomp(iris_num, scale. = T) # standardizing the data 
+pcas
+summary(pcas)
+
+pcas$rotation
+pcas$x
+ pcas_values = as.data.frame(pcas$x)
+ pcas_values$species = iris$Species
+
+ # ploting the vlaues of each 
+ ggplot( pcas_values, aes(PC1, PC2, color = species)) + 
+   geom_point() + 
+   theme_minimal()
+ 
+ ### Supervised Learning ####
+ ## Step 1 : Collect Data ####
+ head(iris)
+
+ ## Step 2: Clean and Process Data ####
+ 
+ #Get Rid of NAs 
+ noNas <- na.omit(starwars) # removes any column with a NA in it 
+ 
+ # Replace with means 
+ replaceWithMean <- mutate(starwars, 
+                           mass = ifelse(is.na(mass), 
+                                         mean(mass),
+                                         mass))
+## Encoding categories as factors or integers 
+ # If categorical vairble is a character, make it a factor
+ intSpecies <- mutate(starwars, 
+                      species = as.integer(as.factor(species)))
+
+ # if categorical variable is already a factor, make it an integer
+ irisAllNumeric <- mutate(iris, 
+                     Species = as.integer(Species))
+View(intIrisSp)
+
+
+## Step 3 : Visualize Data ####
+# Make a PCA -> to see where to data clusters the most 
+# Calculate correlations 
+iriscCors = cor(irisAllNumeric) |>
+  melt()|>
+  as.data.frame()
+
+iriscCors
+
+ggplot(iriscCors, aes(x = Var1, y= Var2, fill = value)) + 
+  geom_tile() +
+  scale_fill_gradient2(low = "red", high = "blue", mid = "white", 
+ 
+                                             midpoint = 0)
+# correlated !! 
+ggplot(irisAllNumeric, aes(x = Petal.Length, y = Sepal.Length)) +
+  geom_count() +
+  theme_minimal()
+
+## Step 4: preform Feature Selection #### 
+# Choose which varibales you want to classify or predcit 
+# Choose which variables you want to use a features in your model 
+# For Iris data..
+# Classify on Species (classifcation) & Predict on Sepal.Lenght (Regression)
+
+# set Seed 
+set.seed(71723)
+
+## Seperate Data into Testing a training sets 
+library(rsample)
+
+#Regression dataset split 
+# put 75% of the data into the training set 
+# create a split 
+reg_split = initial_split(irisAllNumeric, prop = .75)
+
+# Use the split to form testing and training data 
+reg_train = training(reg_split)
+reg_test = testing(reg_split)
+
+# Classification dataset splits ( use iris instead of irisAllNumeric)
+class_split = initial_split(iris, prop = .75)
+
+class_train = training(class_split)
+class_test = testing(class_split)
+
+library(parsnip)
+
+##. Step 6 & 7 ####
+##Linear Regression 
+lm_fit = linear_reg() |>
+  set_engine("lm") |>
+  set_mode("regression") |> 
+  fit(Sepal.Length ~ Petal.Length + Petal.Width + Species + Sepal.Width, data = reg_train)
+
+# Sepal.Lenght = 2.3 + Petal.Length = .797 + Petal.Width = -.4067 + Species = -.3312 + Sepal.Width = 0.5501
+lm_fit
+summary(lm_fit$fit) # close the r^2 value is to 0 the better the model is
+
+
+## Logistic Reg
+log_fit = logistic_reg() |>
+  set_engine("glm") |>
+  set_mode("classification") |>
+  fit(Species ~ Petal.Width + Petal.Length + ., data ~ class_train)
+
+
+## Boosted Tree 
+boost_reg_fit = boost_tree() |>
+  set_engine("xgboost") |> 
+  set_mode("regression") |> 
+  fit(Sepal.Length ~ ., data = reg_train)
+
+boost_reg_fit$fit$evaluation_log
+
+
+# Classifcation 
+boost_class_fit = boost_tree() |>
+  set_engine("xgboost") |> 
+  set_mode("classification") |> 
+  fit(Species ~ ., data = class_train)
+
+boost_class_fit$fit$evaluation_log
+
+# Random Forrest 
+# reg
+forest_reg_fit = rand_forest() |>
+  set_engine("ranger") |> 
+  set_mode("regression") |> 
+  fit(Sepal.Length ~ ., data = reg_train)
+  
+forest_reg_fit$fit
+
+# classification 
+forest_class_fit = rand_forest() |>
+  set_engine("ranger") |> 
+  set_mode("classification") |> 
+  fit(Species ~ ., data = class_train)
+
+forest_class_fit$fit
+
+## Step 8 : evaluate model preformance on Test Set #### 
+
+reg_results = reg_test
+
+# Calculate accuracy for regression  models 
+reg_results$lm_pred = predict(lm_fit, reg_test)$.pred
+reg_results$boost_pred = predict(boost_reg_fit, reg_test)$.pred
+reg_results$forest_pred = predict(forest_reg_fit, reg_test)$.pred
+
+
+yardstick::mae(reg_results, Sepal.Length, lm_pred)
+yardstick::mae(reg_results, Sepal.Length, boost_pred)
+yardstick::mae(reg_results, Sepal.Length, forest_pred)
+
+yardstick::rmse(reg_results, Sepal.Length, lm_pred) 
+yardstick::rmse(reg_results, Sepal.Length, boost_pred)
+yardstick::rmse(reg_results, Sepal.Length, forest_pred)
+
+# Calculate accuracy for classification models 
+
+class_results = class_test
+
